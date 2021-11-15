9e18fb01e7acc556c3cece06ec037ae34634e441
lintobinoy007 <93820158+lintobinoy007@users.noreply.github.com>
2021-11-15 04:40:50 +0000

    beta test

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import logging
 from pytgcalls import idle
 from pyrogram import idle as pidle
 from driver.veez import call_py, bot
+from telegram import Message, Chat, Update, Bot, User
 
 if os.path.exists('log.txt'):
     with open('log.txt', 'r+') as f:
@@ -20,6 +21,16 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 LOGGER = logging.getLogger(__name__)
 
+PM_START_TEXT = """
+
+ʜᴏʟᴀ {}, ᴍʏ ɴᴀᴍᴇ ɪꜱ {}! ɪ ᴀᴍ ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ [ᴛʜᴇ ʟᴇɢᴇɴᴅ](tg://user?id={}) ᴀɴᴅ ɪ ᴀᴍ ᴀ ꜱᴜᴘᴇʀ ᴀᴅᴍɪɴ ʙᴏᴛ.
+
+ɪ ᴀᴍ ᴍᴀᴅᴇᴅ ɪɴ python3 ᴜꜱɪɴɢ ᴛʜɪꜱ ʟɪʙʀᴀʀʏ 𝖕𝖞𝖙𝖍𝖔𝖓-𝖙𝖊𝖑𝖊𝖌𝖗𝖆𝖒-𝖇𝖔𝖙 . ɪ ᴀᴍ ᴛᴏᴛᴀʟʟʏ ᴏᴘᴇɴ ꜱᴏᴜʀᴄᴇ. ᴍʏ ᴄᴏᴅᴇ ɪꜱ ɪɴ ᴛʜɪꜱ ᴡᴇʙ [ʟɪɴᴋ](https://github.com/lintobinoy007/tgbot) ʏᴏᴜ ᴄᴀɴ ꜱᴇᴇ ʜᴇʀᴇ.
+ɪꜰ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴋɴᴏᴡ ᴀᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅ'ꜱ ᴄʟɪᴄᴋ ᴏʀ ᴛʏᴘᴇ /help.
+
+ɪꜰ ʏᴏᴜ ʜᴀᴠᴇ ᴀɴʏ Qᴜᴇʀɪᴇꜱ ᴀʙᴏᴜᴛ ᴛʜᴇ ʙᴏᴛ ᴘʟᴇᴀꜱᴇ ᴄᴏɴᴛᴀᴄᴛ ᴍᴇ (@a_boy_is_no_one00)
+ᴏʀ ᴊᴏɪɴ ᴏᴜʀ ɢʀᴏᴜᴘ (https://t.me/everythingpeople)
+"""
 
 async def mulai_bot():
     print("[VEEZ]: STARTING BOT CLIENT")
